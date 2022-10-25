6bef24f70d6ef5921fd35a54cadd3c5c1076f543
Paula <paula.vitoria@aluno.ifsertao-pe.edu.br>
2022-10-25 15:31:58 -0300

    novo

---
+++ example-app/resources/views/layouts/navigation.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+    <div class="container-fluid">
+        @include('components.application-logo')
+   
+    <div class="d-flex justify-content-end">
+        <a href="#" class="dropdown-toggle btn btn-outline-secondary rounded" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{ Auth::user()->name }}
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <li>
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button class="dropdown-item" type="submit">Sair</button>
+                    </form>
+            <li>
+        </ul>
+       </div>  
+    </div>
+</nav>
